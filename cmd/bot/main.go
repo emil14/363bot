dddc163167f2f8d84f87bfedf596f5d1c6e02133
Mark <mark@getblock.io>
2021-12-16 14:15:44 +0300

    dd

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -40,11 +40,11 @@ func main() {
 	updCfg.Timeout = 60
 	updates := tg.GetUpdatesChan(updCfg)
 
-	go func() {
-		if err := sendDairyMsg(tg); err != nil {
-			panic(err)
-		}
-	}()
+	// go func() {
+	// 	if err := sendDairyMsg(tg); err != nil {
+	// 		panic(err)
+	// 	}
+	// }()
 
 	go func() {
 		if err := startAskJob(ctx, tg); err != nil {
